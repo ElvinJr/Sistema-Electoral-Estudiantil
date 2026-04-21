af2c794f19ca05313ac06faca8200f26ef184b17
Elvin <elvinarmando.inoaromero@gmail.com>
2026-04-21 19:31:08 -0400

    Add database scripts to project (initial integration)

---
+++ SistemadeVotacion/Servidor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Conexion
+{
+    public class Servidor
+    {
+        private readonly string _connectionString;
+
+        public Servidor()
+        {
+            // Conexión típica con Windows Authentication
+            _connectionString = @"Data Source=DESKTOP-VE5IE91\SQLEXPRESS01;Initial Catalog=Votacion;Integrated Security=True;MultipleActiveResultSets=True;";
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public SqlConnection OpenConnection()
+        {
+            var conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ SistemadeVotacion/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Recursos
+{
+    public class Usuarios
+    {
+        public int UsuarioID { get; set; }
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public string Curso { get; set; }
+        public string Seccion { get; set; }
+        public byte[] Contraseña { get; set; }   // hash o bytes
+        public int RolID { get; set; }
+        public int PadronID { get; set; }
+    }
+}
